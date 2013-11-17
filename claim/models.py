ddc1ebf68cf8066de083dcbc311b4f9cde4ad81f
Alykhan Kanji <github@alykhan.com>
2013-11-17 05:04:06 -0500

    Add custom minutes_to_available method

--- claim/models.py
+++ claim/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.utils import timezone
 from django.db import models
 
@@ -11,6 +12,9 @@ class Resource(models.Model):
 
 	def minutes_since_reservation(self):
 		return (timezone.now() - self.reservationTime).seconds / SECONDS_PER_MINUTE
+
+	def minutes_to_available(self):
+		return (self.reservationTime + datetime.timedelta(minutes=self.reservationDuration) - timezone.now()).seconds / SECONDS_PER_MINUTE
 
 	def __unicode__(self):
 		return self.name

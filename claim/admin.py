10841d1551dd7a3f2144af5d599ed48e165db4e5
Alykhan Kanji <github@alykhan.com>
2013-11-17 09:11:33 -0500

    Display Game scores in detail view

--- claim/admin.py
+++ claim/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from claim.models import Resource
+from claim.models import Resource, Game
 
 class ResourceAdmin(admin.ModelAdmin):
 	list_display = ('name', 'available', 'reservationTime')
 	search_fields = ['name']
 
 admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Game)

0fbf3b66c1387c5dea8e8db8eacb548d661cc15a
Alykhan Kanji <github@alykhan.com>
2013-11-17 01:31:20 -0500

    Configure claim admin panel

--- claim/admin.py
+++ claim/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from claim.models import Resource
 
-# Register your models here.
+class ResourceAdmin(admin.ModelAdmin):
+	list_display = ('name', 'available')
+	search_fields = ['name']
+
+admin.site.register(Resource, ResourceAdmin)

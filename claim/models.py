b4cdfd5b41324d4a1deea08a7496857d45255bfc
Alykhan Kanji <github@alykhan.com>
2013-11-17 04:49:48 -0500

    Add reservationDuration field

--- claim/models.py
+++ claim/models.py
@@ -4,6 +4,7 @@ class Resource(models.Model):
 	name = models.CharField(max_length=200)
 	available = models.BooleanField(default=True)
 	reservationTime = models.DateTimeField()
+	reservationDuration = models.PositiveIntegerField(default=15)
 
 	def __unicode__(self):
 		return self.name
